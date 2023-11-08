785766bc4f5457af93d40617bf5ad16011d1d69c
h3ni <djebbiheni01@gmail.com>
2023-11-08 22:22:53 +0100

    react component

---
+++ src/profile/adresse.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Adresse = () => {
+  return <div>adresse</div>;
+};
+
+export default Adresse;
---
+++ src/profile/profilePhoto.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ProfilePhoto = () => {
+  return (
+    <div>
+      <img src="/pp.jpg" alt="myimage" />
+    </div>
+  );
+};
+export default ProfilePhoto;
